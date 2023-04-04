44bf1e194d60eb2478f098e018ce4d09f9928c13
Danni <daniel.ostermann1997@gmail.com>
2023-04-04 18:13:21 +0200

    fix upload behavior

---
+++ EliteInsider/Models/Helpers/AlertStatus.cs
@@ -0,0 +1,10 @@
+﻿using MudBlazor;
+
+namespace EliteInsider.Models.Helpers
+{
+    public class AlertStatus
+    {
+        public Severity Severity { get; set; }
+        public string Text { get; set; }
+    }
+}
